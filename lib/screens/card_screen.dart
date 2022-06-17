ac906f48cae16e5d4d028fb1bcb7226fe389b1b7
chevic <victor.chinenov@gmail.com>
2022-06-17 21:45:58 +0300

    adding singleChildScrollView

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -12,134 +12,133 @@ class CardTeamScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Card(
-            color: Theme.of(context).cardColor,
-            margin: const EdgeInsets.fromLTRB(5, 70, 5, 70),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            elevation: 5,
-            child: Container(
-              margin: const EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    team.name,
-                    style: Theme.of(context).textTheme.headline1,
-                  ),
-                  const SizedBox(
-                    width: 1,
-                    height: 5,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        "location - ",
-                        style: Theme.of(context).textTheme.headline2,
-                      ),
-                      Text(
-                        team.locationName,
-                        style: Theme.of(context).textTheme.headline2,
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Card(
+              color: Theme.of(context).cardColor,
+              margin: const EdgeInsets.fromLTRB(5, 70, 5, 10),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              elevation: 5,
+              child: Container(
+                margin: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      team.name,
+                      style: Theme.of(context).textTheme.headline1,
+                    ),
+                    const SizedBox(
+                      width: 1,
+                      height: 5,
+                    ),
+                    Row(
                       children: [
                         Text(
-                          'first year of play in great league - ',
+                          "location - ",
                           style: Theme.of(context).textTheme.headline2,
                         ),
                         Text(
-                          team.firstYearOfPlay,
-                          style: Theme.of(context).textTheme.headline1,
+                          team.locationName,
+                          style: Theme.of(context).textTheme.headline2,
                         ),
                       ],
                     ),
-                  ),
-                  const SizedBox(
-                    width: 1,
-                    height: 8,
-                  ),
-                  Divider(
-                    color: Theme.of(context).dividerColor,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        "conference: ",
-                        style: Theme.of(context).textTheme.headline4,
-                      ),
-                      Text(
-                        team.conference.name,
-                        style: Theme.of(context).textTheme.headline2,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    width: 1,
-                    height: 5,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        "division: ",
-                        style: Theme.of(context).textTheme.headline4,
-                      ),
-                      Text(
-                        team.division.name,
-                        style: Theme.of(context).textTheme.headline2,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    width: 1,
-                    height: 5,
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        "Franchise: ",
-                        style: Theme.of(context).textTheme.headline3,
-                      ),
-                      Text(
-                        team.franchise.teamName,
-                        style: Theme.of(context).textTheme.headline2,
-                      ),
-                    ],
-                  ),
-                  Divider(
-                    color: Theme.of(context).dividerColor,
-                  ),
-                  Center(
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Icon(
-                        Icons.star,
-                        size: 150,
-                        color: (favorite ? Colors.amber : Colors.grey),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            'first year of play in great league - ',
+                            style: Theme.of(context).textTheme.headline2,
+                          ),
+                          Text(
+                            team.firstYearOfPlay,
+                            style: Theme.of(context).textTheme.headline1,
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                ],
+                    const SizedBox(
+                      width: 1,
+                      height: 8,
+                    ),
+                    Divider(
+                      color: Theme.of(context).dividerColor,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "conference: ",
+                          style: Theme.of(context).textTheme.headline4,
+                        ),
+                        Text(
+                          team.conference.name,
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 1,
+                      height: 5,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "division: ",
+                          style: Theme.of(context).textTheme.headline4,
+                        ),
+                        Text(
+                          team.division.name,
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Franchise: ",
+                          style: Theme.of(context).textTheme.headline3,
+                        ),
+                        Text(
+                          team.franchise.teamName,
+                          style: Theme.of(context).textTheme.headline2,
+                        ),
+                      ],
+                    ),
+                    Divider(
+                      color: Theme.of(context).dividerColor,
+                    ),
+                    Center(
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Icon(
+                          Icons.star,
+                          size: 150,
+                          color: (favorite ? Colors.amber : Colors.grey),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text(
-              'назад',
-              style: Theme.of(context).textTheme.headline1,
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text(
+                'back',
+                style: Theme.of(context).textTheme.headline1,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
